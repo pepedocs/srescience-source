5d75c827a131add214eec49f51c1f27505a228a6
pepedocs <pepedocs@users.noreply.github.com>
2025-08-15 19:21:52 +1000

    init updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://srescience.com',  // Replace with your GitHub Pages URL
+  base: '/',                 // If deployed at username.github.io/repo/
+});
